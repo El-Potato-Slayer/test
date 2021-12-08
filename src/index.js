d063dc08409b3467182b11d09a5ff7518f7eac05
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-12-08 16:53:42 +0200

    stopped tracking .env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import axios from 'axios'
+// require('dotenv').config()
 
+// console.log(env);
+// console.log(dotenv)
+console.log(process.env.HIDDEN_KEY);
 console.log(axios);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const { resolve } = require( 'path' )
+// require('dotenv').config({ path: './.env' }); 
+const Dotenv = require('dotenv-webpack');
 module.exports = {
-entry: './src/index.js',
-module: {
+  entry: './src/index.js',
+  module: {
   rules: [
       {
           test: /\.js$/, //using regex to tell babel exactly what files to transcompile
@@ -10,17 +12,20 @@ module: {
               loader: 'babel-loader' // specify the loader
           } 
       }
-  ]
-},
-output: {
-filename: 'bundle.js',
-path: resolve( __dirname, 'dist' )
-},
-devServer: {
-static: {
-directory: resolve( __dirname, 'dist' )
-}
-},
-target: 'web',
-mode: 'production'
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: resolve( __dirname, 'dist' )
+  },
+  devServer: {
+    static: {
+    directory: resolve( __dirname, 'dist' )
+    }
+  },
+  plugins: [
+    new Dotenv()
+  ],
+  target: 'web',
+  mode: 'production'
 }
