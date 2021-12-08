2e26a2202fadc5c159e04da3ee8fe4d84bc5861a
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-12-08 16:12:07 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const { resolve } = require( 'path' )
 module.exports = {
 entry: './src/index.js',
+module: {
+  rules: [
+      {
+          test: /\.js$/, //using regex to tell babel exactly what files to transcompile
+          exclude: /node_modules/, // files to be ignored
+          use: {
+              loader: 'babel-loader' // specify the loader
+          } 
+      }
+  ]
+},
 output: {
 filename: 'bundle.js',
 path: resolve( __dirname, 'dist' )
@@ -10,6 +21,6 @@ static: {
 directory: resolve( __dirname, 'dist' )
 }
 },
-target: 'node',
+target: 'web',
 mode: 'production'
 }
